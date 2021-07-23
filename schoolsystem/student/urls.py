5d780b74dbcbca1dfa2ea495ca7d72d154bb2f20
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-07-23 19:12:10 +0300

    schoolsystem

---
+++ schoolsystem/student/urls.py
@@ -0,0 +1,7 @@
+from django import urls
+from django.urls import path
+from.views import register_student
+
+urlpatterns=[
+    path("register/",register_student,name="register_student"),
+]
--- schoolsystem/student/views.py
+++ schoolsystem/student/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import StudentRegistrationForm
+
+def register_student(request):
+    form=StudentRegistrationForm()
+    return render(request,"register_student.html",{"form":form})
